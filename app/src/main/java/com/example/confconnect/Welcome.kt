02a2fe83fa11b981838ee2bcd7626b328b2435b4
António Santos <20221966@novaims.unl.pt>
2024-05-19 21:40:07 +0100

    Welcome Page added

--- app/src/main/java/com/example/confconnect/Welcome.kt
+++ app/src/main/java/com/example/confconnect/Welcome.kt
@@ -1,14 +1,18 @@
 package com.example.confconnect
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Welcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+    }
+
+    fun onScreenClicked(view: View) {
+        val intent = Intent(this, Login::class.java)
+        startActivity(intent)
     }
 }
